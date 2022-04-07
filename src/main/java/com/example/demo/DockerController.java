b492c52f77b718f1f7cc1b48128b85bcaa49429d
linsm <lsm06249@itsmycar.cn>
2022-04-07 10:50:04 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DockerController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * File Description
+ *
+ * @author linsm
+ * @date 2022-04-06 16:17
+ */
+@RestController
+public class DockerController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Docker1!";
+    }
+}
